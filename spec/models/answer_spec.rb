2ecc405728628ff9f6d0bc90609241e016a65749
mariyamurashova <mariya09.2009@gmail.com>
2023-10-14 08:04:41 +0300

    Rubocop and dependet destroy were added

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
- it {should belong_to :question}
- it { should validate_presence_of (:body)}
- it { should have_db_index }
+  it { should belong_to :question }
+  it { should validate_presence_of(:body) }
+  it { should have_db_index }
 end
