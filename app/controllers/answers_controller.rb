d2632fc090a33428034e153de2835b1bb9a6a7b1
mariyamurashova <mariya09.2009@gmail.com>
2023-10-20 16:38:20 +0300

    Answer Controller Tests (new and create actions)

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,28 @@
+class AnswersController < ApplicationController
+  before_action :find_question
+
+  def new
+    @answer = @question.answers.new
+  end
+
+  def create
+    @answer = @question.answers.new(answer_params)
+
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :questions
+  resources :questions, shallow: true do 
+    resources :answers, shallow: true 
+  end  
 end
