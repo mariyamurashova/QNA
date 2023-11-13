01639d4e86d5170f649482bd50e6e811ab67c4d1
mariyamurashova <mariya09.2009@gmail.com>
2023-11-13 17:19:43 +0300

    Attach file to Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,6 +3,8 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :author, class_name: "User", foreign_key: :author_id
+  
+  has_one_attached :file
 
   validates :title, :body, presence: true
 end
