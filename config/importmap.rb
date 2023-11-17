500442698e900b1ae0a1cdd3d315c6e14611fe60
Yuriy Kirillov <ykirillov@icloud.com>
2023-11-17 14:16:23 +0300

    storage-fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,10 +7,7 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: false
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 
-pin "jquery", to: "jquery.min.js", preload: true
-pin "jquery_ujs", to: "jquery_ujs.js", preload: true
-
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.1/app/assets/javascripts/rails-ujs.esm.js"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.2/app/assets/javascripts/rails-ujs.esm.js"
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 
