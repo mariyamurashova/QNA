2cbb5bc63a40352b90372ff2f6a2e60ac8bb20f9
mariamurashova <mariya09.2009@gmail.com>
2024-04-11 15:43:40 +0300

    mailer settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.yandex.ru',
+  port:                  465,
+  domain:               'gmail.com',
+  user_name:             Rails.application.credentials[Rails.env.to_sym][:yandex][:email],
+  password:              Rails.application.credentials[Rails.env.to_sym][:yandex][:password],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
